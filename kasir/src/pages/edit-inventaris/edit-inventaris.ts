05b23a4a909bc248f82412643a1e881c2889e913
noer21 <noer.herlambang@gmail.com>
2018-04-14 20:41:15 -0700

    sabtu 15 april 2018

--- kasir/src/pages/edit-inventaris/edit-inventaris.ts
+++ kasir/src/pages/edit-inventaris/edit-inventaris.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, LoadingController, AlertController  } from 'ionic-angular';
+import { Data } from '../../providers/data';
+import { NgForm } from '@angular/forms';
+import { Http } from '@angular/http';
+import { InventarisPage } from '../inventaris/inventaris';
 
 /**
  * Generated class for the EditInventarisPage page.
@@ -15,11 +19,126 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EditInventarisPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  id_barang: any;
+  id_supplier: any;
+  nama_barang: any;
+  nama_supplier: any;
+  harga_barang: any;
+  harga_jual: any;
+  stok_barang: any;
+  temp: any;
+  suppliers: any;
+  submitted = false;
+
+  namaBarang: string;
+  idSupplier: number;
+  hargaJual: number;
+  hargaBarang: number;
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public viewCtrl: ViewController,
+    public loadCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    private data : Data,
+    public http: Http) {
+    this.temp = navParams.get('param1');
+    this.getSupplier();
+
+    this.id_barang = this.temp.item_id;
+    this.id_supplier = this.temp.supplier_id;
+    this.nama_barang = this.temp.item_name;
+    this.nama_supplier = this.temp.supplier_name;
+    this.harga_barang = this.temp.supplier_price;
+    this.harga_jual = this.temp.costumer_price;
+    this.stok_barang = this.temp.item_stock;
+    
+    this.namaBarang = this.nama_barang
+    this.idSupplier= this.id_supplier;
+    this.hargaJual= this.harga_jual;
+    this.hargaBarang= this.harga_barang;
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EditInventarisPage');
+    console.log(this.temp);
+  }
+
+  dismiss(){
+    this.viewCtrl.dismiss();
+  }
+
+  getSupplier(){
+    let loading = this.loadCtrl.create({
+      content: 'memuat..'
+    });
+    
+    loading.present();
+    //api
+      this.http.get(this.data.BASE_URL+"/suppliers_show.php").subscribe(data => {
+      let response = data.json();
+      console.log(response); 
+      if(response.status==200){    
+        this.suppliers = response.data;
+        loading.dismiss();
+      }
+      else {
+        loading.dismiss();
+         let alert = this.alertCtrl.create({
+            title: 'Gagal',
+            subTitle: response.message,      
+            buttons: ['OK']
+          });
+          alert.present();
+      }
+    });
+    //api
+  }
+
+  editInventaris(form: NgForm) {
+    
+    this.submitted = true;
+
+    let loading = this.loadCtrl.create({
+        content: 'memuat..'
+    });
+
+    if(form.valid){
+      
+      loading.present();
+      // api
+      let input = {
+        item_name: this.namaBarang, 
+        id_supplier: this.idSupplier,
+        supplier_price: this.hargaBarang,
+        costumer_price: this.hargaJual
+      };
+        console.log(input, this.id_barang)
+        this.http.post(this.data.BASE_URL+"/items_edit.php?item_id="+this.id_barang, input).subscribe(data => {
+        let response = data.json();
+
+        console.log(response);
+        if(response.status==200){
+          loading.dismiss();
+          this.dismiss();
+          let alert = this.alertCtrl.create({
+            title: 'Berhasil Menambahkan',      
+            buttons: ['OK']
+          });
+          alert.present();
+          this.navCtrl.setRoot(InventarisPage)
+        }
+        else {
+          loading.dismiss();
+          let alert = this.alertCtrl.create({
+              title: 'Gagal Menambahkan',
+              subTitle: 'Silahkan coba lagi',      
+              buttons: ['OK']
+            });
+            alert.present();
+        }      
+      });
+      // api
+    } 
   }
 
 }
--- kasir/src/pages/pembelian/pembelian.ts
+++ kasir/src/pages/pembelian/pembelian.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController,LoadingController,AlertController } from 'ionic-angular';
+import { Data } from '../../providers/data';
+import { NgForm } from '@angular/forms';
+import { Http } from '@angular/http';
 import { CheckOutPage } from '../check-out/check-out';
 
 /**
@@ -15,24 +18,91 @@ import { CheckOutPage } from '../check-out/check-out';
   templateUrl: 'pembelian.html',
 })
 export class PembelianPage {
-  currentNumber=0;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+  n:number;
+  barang:any[] = [];
+  barang_fix:any[] = [];
+  currentNumber:any[];
+  list_search: any;
+  search = false;
+  inventarises: any;
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public modalCtrl: ModalController,
+    private data : Data,
+    public alertCtrl: AlertController,
+    public loadCtrl: LoadingController,
+    public http: Http) {
+      this.getInventaris();
+      this.initiateArray();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PembelianPage');
+    console.log('ionViewDidLoad PembelianPage', this.inventarises);
   }
 
-  private increment () {
-    this.currentNumber++;
+  initiateArray(){
+    for (this.n=0; this.n<=5000; this.n++){
+     this.barang[this.n]=0; 
+    }
+  }
+
+  private increment (index) {
+    this.currentNumber[index]++;
   }
   
-  private decrement () {
-    this.currentNumber--;
+  private decrement (index) {
+    this.currentNumber[index]--;
   }
 
-  checkOut(){
-    this.navCtrl.push(CheckOutPage)
+  barangBeli(value){
+    return value > 0;
+  }
+
+  checkOut(form: NgForm){
+    
+    for(this.n=0; this.n<=5000; this.n++){
+      if(this.barang[this.n]>0){
+        this.barang_fix.push(this.n, this.barang[this.n])
+      }
+    }
+
+    let input = {
+      item_buy: this.barang
+    };
+
+    console.log(this.barang_fix)
+  }
+
+  
+
+  getInventaris(){
+    let loading = this.loadCtrl.create({
+      content: 'memuat..'
+    });
+    
+    loading.present();
+    //api
+      this.http.get(this.data.BASE_URL+"/items_show.php").subscribe(data => {
+      let response = data.json();
+      console.log(response);
+      console.log("array", response[1]); 
+      if(response.status==200){    
+        this.inventarises = response.data;
+        loading.dismiss();
+      }
+      else {
+        loading.dismiss();
+         let alert = this.alertCtrl.create({
+            title: 'Gagal',
+            subTitle: response.message,      
+            buttons: ['OK']
+          });
+          alert.present();
+      }
+    });
+    //api
   }
 
 }
