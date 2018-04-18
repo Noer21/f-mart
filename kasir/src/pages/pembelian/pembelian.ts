03d6f0346a64bf267771e0c9d40851328bc212e0
noer21 <noer.herlambang@gmail.com>
2018-04-18 07:04:51 -0700

    18/04/2018 7.05

--- kasir/src/pages/pembelian/pembelian.ts
+++ kasir/src/pages/pembelian/pembelian.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController,LoadingController,AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController,LoadingController,AlertController, ViewController } from 'ionic-angular';
 import { Data } from '../../providers/data';
 import { NgForm } from '@angular/forms';
 import { Http } from '@angular/http';
@@ -21,17 +21,20 @@ export class PembelianPage {
   
   n:number;
   barang:any[] = [];
-  barang_fix:any[] = [];
-  currentNumber:any[];
-  list_search: any;
-  search = false;
+  barang_fix:any[] = []; //id barang jumlah
   inventarises: any;
+
+  filter_barang_beli:any[] = [];
+  confirm_barang:any[] = []; //nama barang jumlah
+
+  pesan:string;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public modalCtrl: ModalController,
     private data : Data,
     public alertCtrl: AlertController,
+    public viewCtrl: ViewController,
     public loadCtrl: LoadingController,
     public http: Http) {
       this.getInventaris();
@@ -48,31 +51,127 @@ export class PembelianPage {
     }
   }
 
-  private increment (index) {
-    this.currentNumber[index]++;
-  }
-  
-  private decrement (index) {
-    this.currentNumber[index]--;
+  dismiss(){
+    this.viewCtrl.dismiss();
   }
 
   barangBeli(value){
     return value > 0;
   }
 
-  checkOut(form: NgForm){
+  private increment (x) {
+    this.barang[x]++;
+  }
+  
+  private decrement (x) {
+    if(this.barang[x] > 0)
+      this.barang[x]--;
+  }
+
+  checkout_3(){
+    let loading = this.loadCtrl.create({
+      content: 'memuat..'
+    });
+
+    let input = {
+      item_buy: this.barang_fix
+    };
     
+    this.http.post(this.data.BASE_URL+"/cahsier_home.php", input).subscribe(data => {
+      let response = data.json();
+
+      console.log(response);
+      if(response.status==200){
+        loading.dismiss();
+        this.dismiss();
+        let alert = this.alertCtrl.create({
+          title: 'Berhasil Check out',      
+          buttons: ['OK']
+        });
+        alert.present();
+        this.navCtrl.setRoot(PembelianPage)
+      }
+      else {
+        loading.dismiss();
+        let alert = this.alertCtrl.create({
+            title: 'Gagal Menambahkan',
+            subTitle: 'Silahkan coba lagi',      
+            buttons: ['OK']
+          });
+          alert.present();
+      }      
+    });
+    
+
+    
+  }
+
+  checkout_2() {
+    this.pesan = "Anda telah memesan:\n";
+    for(let l =0; l <=5000; l+=2){
+      if(this.confirm_barang[l] == undefined){
+      }
+      else{
+        this.pesan = this.pesan.concat(this.confirm_barang[l]);
+        this.pesan = this.pesan.concat(" sejumlah ");
+        this.pesan = this.pesan.concat(this.confirm_barang[l+1]);
+        this.pesan = this.pesan.concat(" buah.\n");
+      }
+      
+    }
+
+    console.log(this.pesan)
+
+
+    let prompt = this.alertCtrl.create({
+      title: 'Check Out Barang',
+      message: this.pesan,
+      buttons: [
+        {
+          text: 'Konfirmasi',
+          handler: data => {
+            console.log(this.barang_fix)
+            this.navCtrl.setRoot(PembelianPage)
+            //this.checkout_3()
+          }
+        },
+        {
+          text: 'Batal',
+          handler: data => {
+            this.barang_fix = [];
+            this.confirm_barang = [];
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  checkOut_1(form: NgForm){
+    while(this.barang_fix.length > 0) {
+      this.barang_fix.pop();
+  }
+    let i = 0;
     for(this.n=0; this.n<=5000; this.n++){
       if(this.barang[this.n]>0){
         this.barang_fix.push(this.n, this.barang[this.n])
+        this.filter_barang_beli[i] = this.n;
+        i++;
       }
     }
 
-    let input = {
-      item_buy: this.barang
-    };
+    
+    for(let inv of this.inventarises){
+      for (let j = 0; j <= this.filter_barang_beli.length; j++){
+        if(inv.item_id == this.filter_barang_beli[j]){
+          this.confirm_barang.push(inv.item_name, this.barang[inv.item_id]);
+        }
+      }
+    }
 
-    console.log(this.barang_fix)
+    console.log(this.barang_fix, this.filter_barang_beli, this.confirm_barang)
+
+    this.checkout_2()
   }
 
   
